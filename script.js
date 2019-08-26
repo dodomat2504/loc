57fa971e01853e8ae5a9cbab00c32e1830aa8abc
dodomat2504 <domipicturemandok25@gmail.com>
2019-08-26 12:29:03 +0200

    hoffentlich jetzt

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let p = document.getElementById("interagieremitmir");
 
-navigator.geolocation.getCurrentPosition((location) => {
-    console.log(location);
-    p.textContent = "lat: " + location.coords.latitude + " lon: " + location.coords.longitude;
-});
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((location) => {
+        console.log(location);
+        p.textContent = "lat: " + location.coords.latitude + " lon: " + location.coords.longitude;
+    });
+} else {
+    p.textContent = "mooooin";
+}
